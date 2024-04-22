21a5444ea3db3c4eebb38149c071971ed7246f2a
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-04-22 15:17:14 +0200

    main: Add interpreter stub

--- main.cpp
+++ main.cpp
@@ -1,11 +1,43 @@
-#include "Lexer.h"
-
+#include "Parser.h"
 #include <iostream>
+#include <string>
+#include <filesystem>
+namespace fs = std::filesystem;
 
-int main()
+static std::string argv0;
+
+[[noreturn]] void usage(bool error = false)
 {
-    std::string source = "(+){-}";
-    for (const auto& tok : Lox::Lexer(source).lex())
-        std::cout << tok << '\n';
+    (error ? std::cerr : std::cout) <<
+    "Usage: " << argv0 << " [OPTIONS] [FILE]\n"
+    "Without FILE, start repl. Otherwise, eval FILE.\n"
+    "\n"
+    "Options:\n"
+    "  -h, --help    Display this message\n";
+    std::exit(error);
+}
+
+[[noreturn]] void errusage() { usage(true); }
+
+void repl()
+{}
+
+int run(std::string_view)
+{
     return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    using namespace std::string_view_literals;
+    argv0 = fs::path(argv[0]).filename();
+
+    if (argc == 1) {
+        repl();
+        return 0;
+    } else if (argc != 2)
+        errusage();
+    else if (argv[1] == "-h"sv || argv[1] == "--help"sv)
+        usage();
+    return run(argv[1]);
 }
