78d4cfa72522c050c68e82e0d357aa1a26d2a577
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-05-03 20:01:50 +0200

    Move utility classes into Utils.h, Utils.cpp

--- Lexer.h
+++ Lexer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Utils.h"
 #include <variant>
 #include <vector>
 #include <string>
@@ -30,44 +31,6 @@ enum class TokenType {
     Eof,
 };
 
-// 1-based, char-oriented
-struct Position {
-    std::size_t line_num { 0 };
-    std::size_t col_num { 0 };
-
-    bool operator==(const Position&) const = default;
-    bool valid() const { return line_num > 0 && col_num > 0; }
-};
-
-// [start, end) - exclusive
-struct Range {
-    Position start;
-    Position end;
-
-    bool operator==(const Range&) const = default;
-    bool valid() const
-    {
-        return start.valid() && end.valid() && (
-            end.line_num > start.line_num ||
-            (end.line_num == start.line_num &&
-                end.col_num > start.col_num)
-        );
-    }
-};
-
-class SourceMap {
-public:
-    SourceMap(std::string_view source);
-
-    Range span_to_range(std::string_view span);
-    std::string_view line(std::size_t line_num);
-    const std::vector<std::size_t>& line_limits() const { return m_line_limits; }
-
-private:
-    std::vector<std::size_t> m_line_limits;
-    std::string_view m_source;
-};
-
 class Token {
 public:
     using DefaultValueType = std::monostate;
@@ -89,11 +52,6 @@ private:
     TokenType m_type { TokenType::Invalid };
     std::string_view m_text;
     ValueType m_value;
-};
-
-struct Error {
-    std::string_view span;
-    std::string_view msg;
 };
 
 class Lexer {
---
+++ Utils.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace Lox {
+
+// 1-based, char-oriented
+struct Position {
+    std::size_t line_num { 0 };
+    std::size_t col_num { 0 };
+
+    bool operator==(const Position&) const = default;
+    bool valid() const { return line_num > 0 && col_num > 0; }
+};
+
+// [start, end) - exclusive
+struct Range {
+    Position start;
+    Position end;
+
+    bool operator==(const Range&) const = default;
+    bool valid() const
+    {
+        return start.valid() && end.valid() && (
+            end.line_num > start.line_num ||
+            (end.line_num == start.line_num &&
+                end.col_num > start.col_num)
+        );
+    }
+};
+
+class SourceMap {
+public:
+    SourceMap(std::string_view source);
+
+    Range span_to_range(std::string_view span);
+    std::string_view line(std::size_t line_num);
+    const std::vector<std::size_t>& line_limits() const { return m_line_limits; }
+
+private:
+    std::vector<std::size_t> m_line_limits;
+    std::string_view m_source;
+};
+
+struct Error {
+    std::string_view span;
+    std::string_view msg;
+};
+
+}
